e994cd3ba8ab5a2e85b93632d43ac7e5851a8e38
Satyarth Mishra Sharma <mail@satyarth.me>
2015-10-27 03:16:32 +0300

    Add optional arguments to rasterize script

--- rasterize.sh
+++ rasterize.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-inkscape -z -f out.svg -j -e out.png
+if [ -z "$1" ]
+    then
+        input_file='out.svg'
+    else
+        input_file=$1
+fi
+
+if [ -z "$2" ]
+    then
+        output_file='out.png'
+    else
+        output_file=$2
+fi
+
+inkscape -z -f $input_file -j -e $output_file
